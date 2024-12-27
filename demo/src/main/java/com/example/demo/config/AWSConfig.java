484cf33b0a492172af1041d41da68a08a1117541
Kimbomyeong <kbm41890507@gmail.com>
2024-12-27 19:24:04 +0900

    제발

--- demo/src/main/java/com/example/demo/config/AWSConfig.java
+++ demo/src/main/java/com/example/demo/config/AWSConfig.java
@@ -1,30 +1,32 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class AWSConfig {
 
+    @Value("${aws.accesskey}")
+    private String accessKey;
+
+    @Value("${aws.secretkey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        //aws 자격증명 공급자 설정
-        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
-
-        //aws 지역 설정
-        Region region = Region.of(System.getProperty("aws.region", "ap-northeast-2"));
-
-        // DynamoDB 클라이언트를 빌더 패턴을 사용해 생성 및 반환
-        // 자격증명 공급자와 리전을 지정하여 클라이언트를 구성
         return DynamoDbClient.builder()
-                .credentialsProvider(credentialsProvider)
-                .region(region)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
                 .build();
     }
-    
 }
